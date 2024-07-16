68afabe9dad909c6d72c0a97c1cefe10f8c85960
Pang YS <161192932+devbx@users.noreply.github.com>
2024-07-16 14:46:51 +0800

    earth: Include sign keys

--- device.mk
+++ device.mk
@@ -16,6 +16,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+-include vendor/lineage-priv/keys/keys.mk
+
 # A/B
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
