6ebc48006db490d19ff45ebd417e40d86276634b
Pang YS <161192932+devbx@users.noreply.github.com>
2024-08-31 18:36:36 +0800

    earth: Initial TWRP

--- device.mk
+++ device.mk
@@ -6,6 +6,11 @@
 #
 
 LOCAL_PATH := device/xiaomi/earth
+
+# Screen
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1650
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -19,11 +24,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl \
+    update_engine_client
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# MTK PlPath Utils
 PRODUCT_PACKAGES += \
-    bootctrl.mt6768
+    mtk_plpath_utils.recovery
+
+PRODUCT_PACKAGES := \
+    bootctrl.mt6768 \
+    bootctrl.mt6768.recovery
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6768 \
@@ -37,3 +55,26 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.s
