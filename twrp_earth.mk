92b3352e03b640d0bd5422f903ab0f3b52d220d6
Pang YS <161192932+devbx@users.noreply.github.com>
2024-08-31 18:41:46 +0800

    earth: Initial TWRP

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -9,14 +9,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from earth device
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
+
 PRODUCT_DEVICE := earth
-PRODUCT_NAME := omni_earth
+PRODUCT_NAME := twrp_earth
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22120RN86C
 PRODUCT_MANUFACTURER := xiaomi
