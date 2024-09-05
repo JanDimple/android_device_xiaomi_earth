ad6ee9c3460742b340f2342f6ca9f95dfe27179b
Pang YS <161192932+devbx@users.noreply.github.com>
2024-09-05 23:30:48 +0800

    Merge https://github.com/mt6768-dev/android_device_xiaomi_earth/commit/8a93a6a73c5d73d8f1df6551fe1962b908b07cd6 into matrixx

--- device.mk
+++ device.mk
@@ -382,7 +382,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
-    android.hardware.tetheroffload.control@1.1.vendor
+    android.hardware.tetheroffload.control@1.1.vendor \
+    libnetutils.vendor
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
