0b477274c957f4eed6471e53c6339c30f12b9631
Pang YS <161192932+devbx@users.noreply.github.com>
2024-09-08 00:04:38 +0800

    earth: Initial TWRP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_earth.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_earth-user \
-    twrp_earth-userdebug \
-    twrp_earth-eng
+COMMON_LUNCH_CHOICES := twrp_earth-eng
